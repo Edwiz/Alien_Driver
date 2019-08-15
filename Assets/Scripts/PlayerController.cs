72f9b218f365ddb9350b5b6c7c64159bf67922ef
Eduardo M <edmoyronc@gmail.com>
2019-08-15 13:24:47 -0500

    Agregando y probando Tiles, Modificando el movimiento del jugador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
 
-    public Rigidbody2D theRigidBody;
-    public float moveSpeed;
-
-    //public Animator myAnim;
-
+    [Header("Player Configuration")]
+    [Range(0, 10)]
+    public float normalMoveSpeed;
+    [Range(0, 10)]
+    public float diagonalMoveSpeed;
+    [Range(0, 10)]
+    public float acceleration;
+    [Range(1, 10)]
+    public float brakeTime;
     public bool isMovingLeft, isMovingRight, isMovingUp, isMovingDown;
-
     public bool canMove;
+
+    //Cached Variables
+    public Rigidbody2D theRigidBody;
+    public Animator myAnim;
+
+    private float speedIncrement;
+    private float initSpeed;
+    private float xInput;
+    private float yInput;
 
     // Use this for initialization
     void Start()
@@ -24,7 +36,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(theRigidBody.velocity);
 
+        if (speedIncrement > 0 && (Input.GetAxis("Acceleration") == 0 || Input.GetButtonUp("Fire1")))
+        {
+            speedIncrement -= Time.deltaTime * brakeTime;
+        }
     }
 
     private void FixedUpdate()
@@ -34,33 +51,90 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
-        /*
-        //Clamp to horizontal axis avoiding diagonal movement
-        if (isMovingRight || isMovingLeft)
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetAxisRaw("Acceleration") == 1 || Input.GetButton("Fire1"))
         {
-            theRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0f) * moveSpeed;
+            speedIncrement = acceleration;
         }
-        //Clamp to vertical axis avoiding diagonal movement
+
+        // The actual code for moving
+
+        if (isMovingLeft || isMovingRight)
+        {
+            theRigidBody.velocity = new Vector2(xInput, (yInput * diagonalMoveSpeed)) * (normalMoveSpeed + speedIncrement);
+        }
+
         if (isMovingUp || isMovingDown)
         {
-            theRigidBody.velocity = new Vector2(0f, Input.GetAxisRaw("Vertical")) * moveSpeed;
+            theRigidBody.velocity = new Vector2((xInput * diagonalMoveSpeed), yInput) * (normalMoveSpeed + speedIncrement);
         }
-        */
-
-        theRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))* moveSpeed;
 
         //Passing value to animation parameters for movement direction
-        /*myAnim.SetFloat("moveX", theRigidBody.velocity.x);
+        myAnim.SetFloat("moveX", theRigidBody.velocity.x);
         myAnim.SetFloat("moveY", theRigidBody.velocity.y);
 
         //Passing value to animation parameters for Idle animation
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            myAnim.SetFloat("lastMoveX", Input.GetAxis("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxis("Vertical"));
+        }
+
+        //Input without diagonal movement
+        /*
+        //Right Movement
+        if (xInput == 1 && yInput == 0)
+        {
+            theRigidBody.velocity = new Vector2(initSpeed + speedIncrement, 0f);
+        }
+        //Left Movement
+        if (xInput == -1 && yInput == 0)
+        {
+            theRigidBody.velocity = new Vector2(-initSpeed - speedIncrement, 0f);
+        }
+        //Up
+        if (xInput == 0 && yInput == 1)
+        {
+            theRigidBody.velocity = new Vector2(0f, initSpeed + speedIncrement);
+        }
+        //Down Movement
+        if (xInput == 0 && yInput == -1)
+        {
+            theRigidBody.velocity = new Vector2(0f, -initSpeed - speedIncrement);
+        }
+        */
+
+        //CODE FOR DIAGONAL SPEED
+        //Horizontal Movement
+        /*if (isMovingRight || isMovingLeft)
+        {
+            theRigidBody.velocity = new Vector2(xInput * normalMoveSpeed, yInput * diagonalMoveSpeed);
+        }
+
+        //Vertical Movement
+        if (isMovingUp || isMovingDown)
+        {
+            theRigidBody.velocity = new Vector2(xInput * diagonalMoveSpeed, yInput * normalMoveSpeed);
         }*/
 
+        //CODE FOR NOT MOVING IN DIAGONAL
+
         /*
+        //Clamp to horizontal axis avoiding diagonal movement
+        if (isMovingRight || isMovingLeft)
+        {
+            theRigidBody.velocity = new Vector2(xInput, 0) * normalMoveSpeed;
+        }
+
+        //Clamp to vertical axis avoiding diagonal movement
+        if (isMovingUp || isMovingDown)
+        {
+            theRigidBody.velocity = new Vector2(0, yInput) * normalMoveSpeed;
+        }
+        */
+
         //Conditions for preventing diagonal movement
         if (Input.GetAxisRaw("Horizontal") == 1)
         {
@@ -68,6 +142,8 @@ public class PlayerController : MonoBehaviour
             isMovingLeft = false;
             isMovingUp = false;
             isMovingDown = false;
+            //YspeedDefault = Yspeed
+            //YspeedDefault = 0f 
         }
 
         if (Input.GetAxisRaw("Horizontal") == -1)
@@ -93,6 +169,5 @@ public class PlayerController : MonoBehaviour
             isMovingUp = false;
             isMovingDown = true;
         }
-        */
     }
 }
